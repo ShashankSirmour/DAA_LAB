05f1d70d7d133f44b6af5eeb684973823f78ec0e
shashak sirmour <shashank.sirmour11@gmail.com>
2019-02-06 03:22:53 +0530

    Create lab_2_03.c

---
+++ lab2/lab_2_03.c
@@ -0,0 +1,26 @@
+/*
+Q. in given array with  items find the   simultaneously chancge in increasing ordr to decreasing order
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int  KSearch(int arr[],  int size)
+{
+    for (int i = 0; i < size; i++)
+        if(arr[i]>arr[i-1] && arr[i]>arr[i+1])
+            return i;
+}   
+
+int main()
+{
+    int size, key;
+    printf("enter size :");
+    scanf("%d", &size);
+    int *arr = (int *)malloc(sizeof(int) * size);
+    printf("ENTER ARRAY : ");
+    for (int i = 0; i < size; i++)
+        scanf(" %d", &(arr[i]));
+    printf("%d is k",KSearch(arr, size));
+}
