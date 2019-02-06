38fe3cf30fb6c0da4d1c9fc82b4222c97669b466
shashak sirmour <shashank.sirmour11@gmail.com>
2019-02-06 06:44:12 +0530

    Update lab_4_03.c

--- lab4/lab_4_03.c
+++ lab4/lab_4_03.c
@@ -5,9 +5,7 @@ Q. merge sort program
 #include <stdlib.h>
 #include <stdio.h>
 
-// Merges two subarrays of arr[].
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
+
 void merge(int arr[], int l, int m, int r)
 {
     int i, j, k;
